6d256d5ab2423062f7cbb565a97885040020e3f4
Neyunse <neyunse@kagarisoft.com>
2024-10-27 19:56:06 -0300

    up

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -12,24 +12,6 @@ const Main = () => {
 
       <p>The reactivity of Ryunix is similar to Preact, however, it does not pretend to follow any standard of React or any similar Framework, but to allow to generate an SPA in its own way.</p>
 
-      <h2>What is the difference?</h2>
-      <ul>
-        <li>
-          <p>
-            <b>createRoot:</b> was not entered with the same name and usage
-            form, instead it accepts a single string parameter which is the id
-            of the main div of the index.html file.
-          </p>
-        </li>
-        <li>
-          <p>
-            <b>useState:</b> was implemented with another called useStore to make sense
-            when used as a data storage system (not a replacement for
-            localStorage).
-          </p>
-        </li>
-      </ul>
-
       <h2>Getting Started</h2>
       <p>To begin, use the CLI to create a template and start working.</p>
       <Code
