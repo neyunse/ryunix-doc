2d4681a556930df6e8ea3ced368c87b93c44bd09
Neyunse <neyunse@kagarisoft.com>
2025-06-25 17:04:57 -0300

    update ryunix.config.js

--- ryunix.config.js
+++ ryunix.config.js
@@ -1,6 +1,48 @@
 /** @type {import('@unsetsoft/ryunix-webpack/config').Setting} */
 
 const RyunixSettings = {
+  experimental: {
+    ssg: {
+      prerender: [
+        {
+          path: "/doc/introduction",
+          meta: {
+            title: "Introduction | Ryunix Doc"
+          },
+        },
+        {
+          path: "/doc/components",
+          meta: {
+            title: "Components | Ryunix Doc",
+          },
+        },
+        {
+          path: "/doc/styles",
+          meta: {
+            title: "Style & CSS | Ryunix Doc",
+          },
+        },
+        {
+          path: "/doc/components/built-in/image",
+          meta: {
+            title: "Image Component | Ryunix Doc",
+          },
+        },
+        {
+          path: "/doc/components/built-in/navigation",
+          meta: {
+            title: "Navigation | Ryunix Doc",
+          },
+        },
+        {
+          path: "/doc/api/hooks",
+          meta: {
+            title: "Hooks | Ryunix Doc",
+          },
+        },
+      ],
+    },
+  },
   static: {
     favicon: true, // if is false the favicon is not mandatory
     seo: {
