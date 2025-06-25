53f7cbaf50d54f9584d193c5301f0acd3a9a62f2
Neyunse <neyunse@kagarisoft.com>
2025-06-25 16:27:48 -0300

    update package.json, ryunix.config.js and routes.ryx

--- ryunix.config.js
+++ ryunix.config.js
@@ -1,19 +1,63 @@
 /** @type {import('@unsetsoft/ryunix-webpack/config').Setting} */
 
 const RyunixSettings = {
+  experimental: {
+    ssg: {
+      prerender: [
+        {
+          path: "/doc/introduction",
+          meta: {
+            title: "Introduction | Ryunix Doc"
+          },
+        },
+        {
+          path: "/doc/components",
+          meta: {
+            title: "Components | Ryunix Doc",
+          },
+        },
+        {
+          path: "/doc/styles",
+          meta: {
+            title: "Style & CSS | Ryunix Doc",
+          },
+        },
+        {
+          path: "/doc/components/built-in/image",
+          meta: {
+            title: "Image Component | Ryunix Doc",
+          },
+        },
+        {
+          path: "/doc/components/built-in/navigation",
+          meta: {
+            title: "Navigation | Ryunix Doc",
+          },
+        },
+        {
+          path: "/doc/api/hooks",
+          meta: {
+            title: "Hooks | Ryunix Doc",
+          },
+        },
+      ],
+    },
+  },
   static: {
     favicon: true, // if is false the favicon is not mandatory
     seo: {
       title: "Ryunix Doc", // include a default title
-      meta:{
-        "description": "Official RyunixJS documentation an alternative to ReactJs framework",
-        "keywords": "ReactJS, react, Next.js, nextjs, next, RyunixJS, ryunix, ryunix.js, Ryunix.JS"
-      }
+      meta: {
+        description:
+          "Official RyunixJS documentation an alternative to ReactJs framework",
+        keywords:
+          "ReactJS, react, Next.js, nextjs, next, RyunixJS, ryunix, ryunix.js, Ryunix.JS",
+      },
     },
   },
   webpack: {
     production: true, // use 'false' for dev mode
-  }
-}
+  },
+};
 
-export default RyunixSettings
+export default RyunixSettings;
